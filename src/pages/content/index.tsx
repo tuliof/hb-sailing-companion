8d925ec404262e76d8a9c3b7e3400a16b09eeada
Tulio Fernandes <tulio.fernandes@gmail.com>
2025-04-19 17:07:14 -0400

    fix issue with page styles clashing with extension component styling

--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -1,20 +1,30 @@
 import React from 'react'
-import './content.css' // Import the content-specific CSS
 import ReactDOM from 'react-dom/client'
 import { Content } from './content'
+import styles from './content.css?inline' // Import the content-specific CSS
 
 // Create a root div and render the App component into it
-const root = document.createElement('div')
-root.className = 'container'
-root.id = 'hb-sailing-companion'
+const mountPoint = document.createElement('div')
+mountPoint.id = 'hb-sailing-companion'
+mountPoint.attachShadow({ mode: 'open' })
+
+const target: ShadowRoot | null = mountPoint.shadowRoot
+if (!target) {
+	throw Error('No target element found, cannot attach shadow root')
+}
+
+const rootDiv = ReactDOM.createRoot(target)
+
 const anchor = document.getElementsByClassName('rbm_All')[0]
 if (!anchor) {
 	throw new Error('No anchor element found')
 }
-anchor.insertAdjacentElement('beforebegin', root)
-const rootDiv = ReactDOM.createRoot(root)
+anchor.insertAdjacentElement('beforebegin', mountPoint)
 rootDiv.render(
 	<React.StrictMode>
-		<Content />
+		<>
+			<style>{styles.toString()}</style>
+			<Content />
+		</>
 	</React.StrictMode>,
 )
